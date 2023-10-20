27d376c14a1be470de64db5d35fee7bb939e0545
chuyeee <yj2000325@gmail.com>
2023-10-20 10:09:46 +0900

    [FIX] answer 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from './store';
 import App from './App';
 import { Provider } from 'react-redux';
 
@@ -7,9 +8,9 @@ window.API_BASE_URL = `https://foralpha.shinhan.site`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-
+  </Provider>
 );
